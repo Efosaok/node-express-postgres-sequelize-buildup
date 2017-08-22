4c1e4905b805f3293abde4bee22bd213613b3cbc
efosa <efosaokpugie@gmail.com>
2017-08-22 22:11:54 +0100

    root app

---
+++ app.js
@@ -0,0 +1,23 @@
+// require all necessary dependencies||
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const app = express();
+
+app.set('port', process.env.PORT || 8000);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => {
+  res.send({ message: 'Hello,It is just me' });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('app running on port ' + app.get('port'));
+});
+
+module.exports = app;
